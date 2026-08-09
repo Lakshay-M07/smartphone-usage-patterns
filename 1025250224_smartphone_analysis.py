6e73dd656987197defd901a5c0ae8576a580af8b
Lakshay-M07 <lakshay.mohata@gmail.com>
2026-08-09 14:43:57 +0530

    Add smartphone usage analysis script

---
+++ 1025250224_smartphone_analysis.py
@@ -0,0 +1,13 @@
+# =============================================================================
+# DATA ANALYSIS PROJECT — 
+# Name - Lakshay Mohata
+# Dataset: Smartphone Usage and Addiction Analysis (7500 Records)
+# =============================================================================
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+plt.rcParams.update({
+    "figure.dpi": 130, "axes.titlesize": 13, "axes.labelsize": 11,
+    "xtick.labelsize": 9, "yt
